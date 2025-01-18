e4c4ac81d5a549741e8949c8d1b4ae87f6aafa58
Raushan Iangirov <mandarinko999@gmail.com>
2025-01-18 17:18:40 +0300

    add loki, github actions

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,7 +13,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
 	config.resolve?.modules?.push(paths.src);
 	config.resolve?.extensions?.push('.ts', '.tsx');
 
-	config.module?.rules? = config.module.rules.map((rule) => {
+	// @ts-ignore
+	config.module.rules = config.module.rules.map((rule) => {
 		if (/svg/.test(rule.test as string)) {
 			return { ...rule, exclude: /\.svg$/i };
 		}
